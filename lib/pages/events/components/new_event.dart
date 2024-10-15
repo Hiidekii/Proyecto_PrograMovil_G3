8220faab042972d2849582036ffa415bb23052a0
LuisMartinez <20211628@aloe.ulima.edu.pe>
2024-10-15 01:07:24 -0500

    change

--- lib/pages/events/components/new_event.dart
+++ lib/pages/events/components/new_event.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:proyecto_programovil_g3/configs/colors.dart';
 
 class NewEvent extends StatelessWidget {
   @override
@@ -10,35 +11,62 @@ class NewEvent extends StatelessWidget {
           left: 16,
           right: 16,
           top: 16),
-      child: SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.zero,
+            bottomRight: Radius.zero,
+          ),
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+            // Título "Nuevo evento"
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Nuevo evento',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.darkBackgroundColor,
+                ),
+              ),
+            ),
+            // Categoría
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.min,
               children: [
-                _categoryButton(context, 'Reu', Colors.orange),
-                _categoryButton(context, 'Cumple', Colors.blue),
-                _categoryButton(context, 'After', Colors.pink),
+                _categoryButton(
+                    context, 'Reu', Colors.orange, CupertinoIcons.person_2),
+                SizedBox(width: 8),
+                _categoryButton(
+                    context, 'Cumple', Colors.blue, CupertinoIcons.piano),
+                SizedBox(width: 8),
+                _categoryButton(
+                    context, 'After', Colors.pink, CupertinoIcons.hifispeaker),
               ],
             ),
             SizedBox(height: 16),
-            _buildTextField(CupertinoIcons.pencil, 'Título'),
-            SizedBox(height: 8),
+            _buildTextField(CupertinoIcons.tickets, 'Título'),
+            SizedBox(height: 2),
             _buildTextField(CupertinoIcons.calendar, 'Fecha'),
-            SizedBox(height: 8),
+            SizedBox(height: 2),
             _buildTextField(CupertinoIcons.time, 'Hora'),
-            SizedBox(height: 8),
+            SizedBox(height: 2),
             _buildTextField(CupertinoIcons.text_alignleft, 'Descripción'),
-            SizedBox(height: 8),
-            _buildTextField(CupertinoIcons.location, 'Ubicación'),
-            SizedBox(height: 8),
+            SizedBox(height: 2),
+            _buildTextField(CupertinoIcons.location_solid, 'Ubicación'),
+            SizedBox(height: 2),
             _buildTextField(CupertinoIcons.music_note, 'Link de playlist'),
-            SizedBox(height: 8),
+            SizedBox(height: 2),
             _buildTextField(CupertinoIcons.chat_bubble_text, 'Link de chat'),
             SizedBox(height: 16),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 ElevatedButton(
                   onPressed: () {
@@ -47,6 +75,7 @@ class NewEvent extends StatelessWidget {
                   child: Text('Cancelar'),
                   style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
                 ),
+                SizedBox(width: 8),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.pop(context);
@@ -55,6 +84,7 @@ class NewEvent extends StatelessWidget {
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.orange[700]),
                 ),
+                SizedBox(width: 16),
               ],
             ),
             SizedBox(height: 16),
@@ -64,21 +94,35 @@ class NewEvent extends StatelessWidget {
     );
   }
 
-  Widget _categoryButton(BuildContext context, String text, Color color) {
+  Widget _categoryButton(
+      BuildContext context, String text, Color color, IconData icon) {
     return ElevatedButton(
       onPressed: () {},
-      child: Text(text),
       style: ElevatedButton.styleFrom(backgroundColor: color),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 16, color: Colors.black),
+          SizedBox(width: 8),
+          Text(text),
+        ],
+      ),
     );
   }
 
   Widget _buildTextField(IconData icon, String hintText) {
     return TextField(
       decoration: InputDecoration(
-        prefixIcon: Icon(icon),
+        prefixIcon: Icon(icon, color: AppColors.darkBackgroundColor),
         hintText: hintText,
-        border: OutlineInputBorder(),
+        hintStyle: TextStyle(color: Colors.grey),
+        filled: true,
+        fillColor: const Color.fromARGB(255, 255, 255, 255),
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
       ),
+      style: TextStyle(color: Colors.grey),
     );
   }
 }
@@ -87,6 +131,7 @@ void showNewEventModal(BuildContext context) {
   showModalBottomSheet(
     context: context,
     isScrollControlled: true,
+    backgroundColor: Colors.transparent,
     builder: (BuildContext context) {
       return NewEvent();
     },
